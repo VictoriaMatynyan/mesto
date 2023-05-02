5bfbade4a7fce3b387ebecbf21d85c91263ec906
Victoria Matynyan <the.strange.creature@yandex.ru>
2023-05-02 13:25:56 +0300

    first corrections after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,23 @@
 const popupButtonOpener = document.querySelector('.profile__popup-open');
 const popup = document.querySelector('.popup');
-const popupCloseSymbol = document.querySelector('.popup__close-icon');
+const popupCloseSymbol = document.querySelector('.popup__close-button');
 
-const togglePopupStatus = (popupIntoToggle) => popupIntoToggle.classList.toggle('popup__opened');
+const togglePopupStatus = (popupIntoToggle) => popupIntoToggle.classList.toggle('popup_opened');
 popupButtonOpener.addEventListener('click', () => togglePopupStatus(popup))
+popupCloseSymbol.addEventListener('click', () => togglePopupStatus(popup)
+)
 
-popupCloseSymbol.addEventListener('click', () => togglePopupStatus(popup))
-
-popup.addEventListener('click', (evt) => {
-    if (evt.target === evt.currentTarget) {
-        togglePopupStatus(popup)
-    }
-})
+// popup.addEventListener('click', (evt) => {
+//     if (evt.target === evt.currentTarget) {
+//         togglePopupStatus(popup)
+//     }
+// }) функция закрытия окна popup при нажатии на область вне окна
 
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const popupInputName = document.querySelector('.popup__input-name');
-const popupInputDescription = document.querySelector('.popup__input-description');
+const formElement = document.querySelector('.popup__input-container');
+const popupInputName = formElement.querySelector('.popup__input_name');
+const popupInputDescription = formElement.querySelector('.popup__input_description');
 const submitButton = document.querySelector('.popup__submit-button');
 
 popupCloseSymbol.addEventListener('click', () => {
@@ -35,17 +36,6 @@ submitButton.addEventListener('click', () => {
     profileDescription.textContent = profileSubmitDescription;
     togglePopupStatus(popup);
 })
-
-const likeButtons = document.querySelectorAll('.element__like-button');
-likeButtons.forEach((likeButton) => {
-    likeButton.classList.remove ('element__like-button_active');
-    likeButton.addEventListener('click', () => {
-    likeButton.classList.toggle('element__like-button_active'),
-    likeButton.classList.toggle('element__like-button')}
-    );
-})
-
-const formElement = document.querySelector('.popup__input-container'); 
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
