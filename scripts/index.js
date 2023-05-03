2b43cb5f726f82d3025fd5c523b8e4d0850a921a
Victoria Matynyan <the.strange.creature@yandex.ru>
2023-05-03 16:39:35 +0300

    second corrections after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,41 +1,30 @@
 const popupButtonOpener = document.querySelector('.profile__popup-open');
 const popup = document.querySelector('.popup');
 const popupCloseSymbol = document.querySelector('.popup__close-button');
-
-const togglePopupStatus = (popupIntoToggle) => popupIntoToggle.classList.toggle('popup_opened');
-popupButtonOpener.addEventListener('click', () => togglePopupStatus(popup))
-popupCloseSymbol.addEventListener('click', () => togglePopupStatus(popup)
-)
-
-// popup.addEventListener('click', (evt) => {
-//     if (evt.target === evt.currentTarget) {
-//         togglePopupStatus(popup)
-//     }
-// }) функция закрытия окна popup при нажатии на область вне окна
-
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
 const formElement = document.querySelector('.popup__input-container');
-const popupInputName = formElement.querySelector('.popup__input_name');
-const popupInputDescription = formElement.querySelector('.popup__input_description');
-const submitButton = document.querySelector('.popup__submit-button');
+const popupInputName = formElement.querySelector('.popup__input_type_name');
+const popupInputDescription = formElement.querySelector('.popup__input_type_description');
+
+const togglePopupStatus = (popupIntoToggle) => popupIntoToggle.classList.toggle('popup_opened');
+
+popupButtonOpener.addEventListener('click', () => {
+    togglePopupStatus(popup);
+    popupInputName.value = profileName.textContent;
+    popupInputDescription.value = profileDescription.textContent;
+})
 
 popupCloseSymbol.addEventListener('click', () => {
-    const popupName = popupInputName.value;
-    const popupDescription = popupInputDescription.value;
+    togglePopupStatus(popup);
     popupInputName.value = '';
     popupInputDescription.value = '';
-    profileName.value = '';
-    profileDescription.value = '';
-});
-
-submitButton.addEventListener('click', () => {
-    const profileSubmitName = popupInputName.value;
-    const profileSubmitDescription = popupInputDescription.value;
-    profileName.textContent = profileSubmitName;
-    profileDescription.textContent = profileSubmitDescription;
-    togglePopupStatus(popup);
 })
+
+// popup.addEventListener('click', (evt) => {
+//     if (evt.target === evt.currentTarget) {
+//         togglePopupStatus(popup)}
+// }) функция закрытия окна popup при нажатии на область вне окна
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
@@ -43,6 +32,7 @@ function handleFormSubmit (evt) {
     const descriptionInput = popupInputDescription.value;
     profileName.textContent = popupInputName.value;
     profileDescription.textContent = popupInputDescription.value;
+    togglePopupStatus(popup);
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
